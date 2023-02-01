836417f9dc1194f818ff40147baa3ce2ca5be55d
ProgrammXCo <ProgrammX-python@yandex.ru>
2023-02-01 17:48:52 +0300

    skibidi dop dop dop yes yes

--- skibidi dop dop dop yes yes.py
+++ skibidi dop dop dop yes yes.py
@@ -1,9 +1,11 @@
 from PIL import Image, ImageTk
 import tkinter as tk
 import time
-import sys
+import threading
 from pygame import mixer
 from itertools import cycle
+import signal
+import os
  
 class GifLabel(tk.Label):
     def load(self, im):
@@ -34,28 +36,28 @@ class GifLabel(tk.Label):
 
 mixer.init()
 
-class Main():
-    def __init__(self, root: tk.Tk):
+class Main(tk.Tk):
+    def __init__(self):
         self.path_icon = r"skibidi dop dop dop yes yes.ico"
-
-        self.root = root
-        self.root.geometry("600x600")
-        self.root.state("zoomed")
-        self.root.title("skibidi dop dop dop yes yes")
-        self.root.iconbitmap(self.path_icon)
-
-        self.root.protocol("WM_DELETE_WINDOW", self.exit_window)
-
-        self.canvas = tk.Canvas(root, bg = "white", highlightthickness=0)
-        self.canvas.pack(fill = "both", expand = True)
-
         self.path_sound = r"skibidi dop dop dop yes yes.mp3"
         self.path_gif = r"skibidi dop dop dop yes yes.gif"
-        self.path_bg = r"bg.jfif"
+        self.path_bg = r"bg.jpg"
+
+        super().__init__()
+
+        self.geometry("900x500")
+        self.state("zoomed")
+        self.title("skibidi dop dop dop yes yes")
+        self.iconbitmap(self.path_icon)
+
+        self.protocol("WM_DELETE_WINDOW", self.exit_window)
+
+        self.canvas = tk.Canvas(self, bg = "white", highlightthickness=0)
+        self.canvas.pack(fill = "both", expand = True)
         
         self.image_bg = Image.open(self.path_bg).convert("RGBA")
 
-        self.root.update()
+        self.update()
         
         self.width = self.canvas.winfo_width()
         self.height = self.canvas.winfo_height()
@@ -63,29 +65,49 @@ class Main():
         self.picture_bg = ImageTk.PhotoImage(self.image_bg.resize((self.width, self.height), Image.Resampling.LANCZOS))
         self.picture_bg_tk = self.canvas.create_image(0, 0, image=self.picture_bg, anchor="nw")
 
+        self.canvas.bind("<Configure>", self.scale_image)
+
         self.gif = GifLabel(self.canvas, borderwidth = 0)
         self.gif.place(x = 0, y = 0)
         self.gif.load(self.path_gif)
         self.gif.bind("<ButtonPress-1>", self.play_music)
 
-        self.root.update()
+        self.update()
 
         self.gif_width = self.gif.winfo_width()
         self.gif_height = self.gif.winfo_height()
+
+        self.minsize(self.gif_width + 100, self.gif_height + 100)
 
         self.xspeed = 1
         self.yspeed = 1
 
         self.closing = False
 
-        self.root.update()
-        
+        self.thread = threading.Thread(target = self.start)
+        self.thread.start()
+
+        self.mainloop()
+
+    def start(self):
+        self.update()
         while not self.closing:
+            
             self.move()
-            self.root.update()
+            self.update()
             time.sleep(0.003)
 
-        sys.exit()
+        self.exit_programm()
+
+    def exit_programm(self):
+        os.kill(os.getpid(), signal.SIGINT)
+
+    def scale_image(self, event):
+        self.width = self.canvas.winfo_width()
+        self.height = self.canvas.winfo_height()
+
+        self.picture_bg = ImageTk.PhotoImage(self.image_bg.resize((self.width, self.height), Image.Resampling.LANCZOS))
+        self.canvas.itemconfig(self.picture_bg_tk, image = self.picture_bg)
         
     def move(self):
         self.gif.place(x = self.gif.winfo_x() + self.xspeed, y = self.gif.winfo_y() + self.yspeed)
@@ -114,8 +136,4 @@ class Main():
         self.closing = True
 
 if __name__ == "__main__":
-    root = tk.Tk()
-
-    Main(root)
-
-    root.mainloop()
+    Main()
